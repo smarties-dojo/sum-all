14d19ee8664a64663763ec5564fe6ba39b4faa29
Matthias Harrer <matthias.harrer@de.trumpf.com>
2017-12-05 15:54:06 +0100

    Parametrisierte Tests

--- tests/SumAllTests.cs
+++ tests/SumAllTests.cs
@@ -7,74 +7,20 @@ namespace tests
 {
     public class SumAllTests
     {
-        [Fact]
-        public void Given_1_and_4_should_return_10()
+        [Theory]
+        [InlineData(1, 4, 10)]
+        [InlineData(4, 1, 10)]
+        [InlineData(5, 10, 45)]
+        [InlineData(10, 5, 45)]
+        [InlineData(5, 5, 10)]
+        [InlineData(-5, 5, 0)]
+        public void Given_two_numbers_should_return_the_sum_of_those_and_all_inbetween(int first, int second, int expected)
         {
-            // Arrange
-            var first = 1;
-            var second = 4;
-
             // Act
             var result = Program.SumAll(first, second);
 
             // Assert
-            result.Should().Be(10);
-        }
-
-        [Fact]
-        public void Given_4_and_1_should_return_10()
-        {
-            // Arrange
-            var first = 4;
-            var second = 1;
-
-            // Act
-            var result = Program.SumAll(first, second);
-
-            // Assert
-            result.Should().Be(10);
-        }
-
-        [Fact]
-        public void Given_5_and_10_should_return_45()
-        {
-            // Arrange
-            var first = 5;
-            var second = 10;
-
-            // Act
-            var result = Program.SumAll(first, second);
-
-            // Assert
-            result.Should().Be(45);
-        }
-
-        [Fact]
-        public void Given_10_and_5_should_return_45()
-        {
-            // Arrange
-            var first = 10;
-            var second = 5;
-
-            // Act
-            var result = Program.SumAll(first, second);
-
-            // Assert
-            result.Should().Be(45);
-        }
-
-        [Fact]
-        public void Given_5_and_5_should_return_10()
-        {
-            // Arrange
-            var first = 5;
-            var second = 5;
-
-            // Act
-            var result = Program.SumAll(first, second);
-
-            // Assert
-            result.Should().Be(10);
+            result.Should().Be(expected);
         }
     }
 }
