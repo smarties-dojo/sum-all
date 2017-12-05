a1683d6f275200ee5fda22476afa322ce79c66cd
David Randler <david.randler@gmail.com>
2017-12-05 20:49:18 +0100

    handle case of two identical input values

--- fsharp/implementation/SumAllNumbers/Library.fs
+++ fsharp/implementation/SumAllNumbers/Library.fs
@@ -3,5 +3,10 @@ namespace SumAllNumbers
 module Library =
     let SumAll first second =
         let step = if first < second then 1 else -1
-        [first..step..second]
+        let allNumbers = 
+            if first = second 
+                then [first; second] 
+                else [first..step..second]
+        
+        allNumbers
         |> List.sum
