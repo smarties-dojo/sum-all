4b2a51e367275b8772df9205d2d9100c24838db5
Matthias Harrer <matthias.harrer@de.trumpf.com>
2017-12-05 14:32:58 +0100

    Generate empty console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumAllNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
