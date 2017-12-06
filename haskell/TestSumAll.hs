dd05f48189fb364a225a4f8b54bc80ff92ca0c67
Matthias Harrer <matthias.harrer@de.trumpf.com>
2017-12-06 11:40:01 +0100

    fix test descriptions

--- haskell/TestSumAll.hs
+++ haskell/TestSumAll.hs
@@ -15,22 +15,22 @@ test4And1ShouldEqual10 =
 
 test5And5ShouldEqual10 :: Test
 test5And5ShouldEqual10 =
-    TestCase $ assertEqual "Given 4 and 1 should return 10"
+    TestCase $ assertEqual "Given 5 and 5 should return 10"
                             10 (sumall 5 5)
 
 test5And10ShouldEqual45 :: Test
 test5And10ShouldEqual45 =
-    TestCase $ assertEqual "Given 4 and 1 should return 10"
+    TestCase $ assertEqual "Given 5 and 10 should return 45"
                             45 (sumall 5 10)
 
 test10And5ShouldEqual45 :: Test
 test10And5ShouldEqual45 =
-    TestCase $ assertEqual "Given 4 and 1 should return 10"
+    TestCase $ assertEqual "Given 10 and 5 should return 45"
                             45 (sumall 10 5)
 
 test5AndMinus5ShouldEqual0 :: Test
 test5AndMinus5ShouldEqual0 =
-    TestCase $ assertEqual "Given 4 and 1 should return 10"
+    TestCase $ assertEqual "Given 5 and -5 should return 0"
                             0 (sumall 5 (-5))
 
 main :: IO Counts
