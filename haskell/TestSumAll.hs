fe3970b96da2f5d5bcfd9351e05fcf9c80eabad9
Matthias Harrer <matthias.harrer@de.trumpf.com>
2017-12-06 09:33:44 +0100

    Add haskell tests

---
+++ haskell/TestSumAll.hs
@@ -0,0 +1,44 @@
+module TestSafePrelude where
+
+import Test.HUnit
+import SumAll
+
+test1And4ShouldEqual10 :: Test
+test1And4ShouldEqual10 =
+    TestCase $ assertEqual "Given 1 and 4 should return 10"
+                           10 (sumall 1 4)
+
+test4And1ShouldEqual10 :: Test
+test4And1ShouldEqual10 =
+    TestCase $ assertEqual "Given 4 and 1 should return 10"
+                            10 (sumall 4 1)
+
+test5And5ShouldEqual10 :: Test
+test5And5ShouldEqual10 =
+    TestCase $ assertEqual "Given 4 and 1 should return 10"
+                            10 (sumall 5 5)
+
+test5And10ShouldEqual45 :: Test
+test5And10ShouldEqual45 =
+    TestCase $ assertEqual "Given 4 and 1 should return 10"
+                            45 (sumall 5 10)
+
+test10And5ShouldEqual45 :: Test
+test10And5ShouldEqual45 =
+    TestCase $ assertEqual "Given 4 and 1 should return 10"
+                            45 (sumall 10 5)
+
+test5AndMinus5ShouldEqual0 :: Test
+test5AndMinus5ShouldEqual0 =
+    TestCase $ assertEqual "Given 4 and 1 should return 10"
+                            0 (sumall 5 (-5))
+
+main :: IO Counts
+main = runTestTT $ TestList
+    [ test1And4ShouldEqual10
+    , test4And1ShouldEqual10
+    , test5And5ShouldEqual10
+    , test5And10ShouldEqual45
+    , test10And5ShouldEqual45
+    , test5AndMinus5ShouldEqual0
+    ]
